14e969b8f188c92430c8b7dbb513231090e42a48
RohithV16 <Rohithv16@gmail.com>
2024-09-21 23:01:35 +0530

    Logged only params

--- actions/generic/index.js
+++ actions/generic/index.js
@@ -1,40 +1,35 @@
 const fetch = require("node-fetch");
 const { Core } = require("@adobe/aio-sdk");
 const {
-errorResponse,
-getBearerToken,
-stringParameters,
-checkMissingRequestInputs,
+    errorResponse,
+    stringParameters,
 } = require("../utils");
 
 // main function that will be executed by Adobe I/O Runtime
 async function main(params) {
-// create a Logger
-const logger = Core.Logger("main", { level: params.LOG_LEVEL || "info" });
+    // create a Logger
+    const logger = Core.Logger("main", { level: params.LOG_LEVEL || "info" });
 
-try {
-    // 'info' is the default level if not set
-    logger.info("Calling the main action");
+    try {
+        // 'info' is the default level if not set
+        logger.info("Calling the main action");
 
-    // log parameters, only if params.LOG_LEVEL === 'debug'
-    logger.debug(stringParameters(params));
+        // log parameters, only if params.LOG_LEVEL === 'debug'
+        logger.debug(stringParameters(params));
 
-    const response = {
-    statusCode: 200,
-    body: {
-        message: "Received AEM Event, it will be processed in next example",
-    },
-    };
+        const response = {
+            param: params
+        };
 
-    // log the response status code
-    logger.info(`${response.statusCode}: successful request`);
-    return response;
-} catch (error) {
-    // log any server errors
-    logger.error(error);
-    // return with 500
-    return errorResponse(500, "server error", logger);
-}
+        // log the response status code
+        logger.info(`${response.statusCode}: successful request`);
+        return response;
+    } catch (error) {
+        // log any server errors
+        logger.error(error);
+        // return with 500
+        return errorResponse(500, "server error", logger);
+    }
 }
 
 exports.main = main;
